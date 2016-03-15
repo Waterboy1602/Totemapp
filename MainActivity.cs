db5b027c14dd61c4420cf1a32595eca730d768be
Frederick Eskens <fe@vvksm.local>
2016-03-15 09:49:54 +0100

    Adjusted menu and reuploaded drawables

--- MainActivity.cs
+++ MainActivity.cs
@@ -18,7 +18,6 @@ namespace Totem
 	[Activity (Label = "Totem", MainLauncher = true, Icon = "@drawable/icon", Theme = "@style/AppThemeNoAction")]
 	public class MainActivity : Activity
 	{
-		Button totems;
 		Database db;
 
 		protected override void OnCreate (Bundle bundle) {
@@ -28,8 +27,8 @@ namespace Totem
 
 			SetContentView (Resource.Layout.Main);
 
-			totems = FindViewById<Button> (Resource.Id.totems);
-			Button totemBepalen = FindViewById<Button> (Resource.Id.totemBepalen);
+			Button totems = FindViewById<Button> (Resource.Id.totems);
+			Button eigenschappen = FindViewById<Button> (Resource.Id.eigenschappen);
 			Button profielen = FindViewById<Button> (Resource.Id.profielen);
 
 			if(db.GetPreference("tips").value.Equals("true")) {
@@ -37,39 +36,20 @@ namespace Totem
 			}
 
 			totems.Click += (sender, eventArgs) => GoToActivity("totems");
-			totemBepalen.Click += (sender, eventArgs) => GoToActivity("bepalen");
+			eigenschappen.Click += (sender, eventArgs) => GoToActivity("eigenschappen");
 			profielen.Click += (sender, eventArgs) => GoToActivity("profielen");
 		}
 
-		private void GoToActivity(string a) {
-			if(a.Equals("totems")) {
-				TotemLijst();
-			} else if(a.Equals("bepalen")) {
-				Intent intent = new Intent(this, typeof(EigenschappenActivity));
-				StartActivity (intent);
-			} else if(a.Equals("profielen")) {
-				Intent intent = new Intent(this, typeof(ProfielenActivity));
-				StartActivity (intent);
+		private void GoToActivity(string activity) {
+			Intent intent = null;
+			if(activity.Equals("totems")) {
+				intent = new Intent(this, typeof(AllTotemsActivity));
+			} else if(activity.Equals("eigenschappen")) {
+				intent = new Intent(this, typeof(AllEigenschappenActivity));
+			} else if(activity.Equals("profielen")) {
+				intent = new Intent(this, typeof(ProfielenActivity));
 			}
-		}
-
-		private void TotemLijst() {
-			PopupMenu menu = new PopupMenu (this, totems);
-			menu.Inflate (Resource.Menu.Popup);
-			menu.Menu.Add(0,1,1,"Zoeken op totem");
-			menu.Menu.Add(0,2,2,"Zoeken op eigenschappen");
-			menu.MenuItemClick += (s1, arg1) => {
-				if(arg1.Item.TitleFormatted.ToString().Equals("Zoeken op totem")) {
-					Intent intent = new Intent(this, typeof(AllTotemsActivity));
-					StartActivity (intent);				
-				}
-				else {
-					Intent intent = new Intent(this, typeof(AllEigenschappenActivity));
-					StartActivity (intent);				
-				}
-			};
-
-			menu.Show ();
+			StartActivity (intent);
 		}
 
 		public void ShowTipDialog() {
