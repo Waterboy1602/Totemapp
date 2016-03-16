0d7846b76ba183305d56013ca0b9bd6f6d0dd82c
Frederick Eskens <fe@vvksm.local>
2016-03-16 12:39:50 +0100

    Added CustomFontHelper

--- Utilities/CustomFontTextView.cs
+++ Utilities/CustomFontTextView.cs
@@ -12,55 +12,15 @@ namespace Totem {
 		public static string ANDROID_SCHEMA = "http://schemas.android.com/apk/res/android";
 
 		public CustomFontTextView (Context context): base(context) {
-			ApplyCustomFont (context, null);
+			CustomFontHelper.ApplyCustomFont (this, context, null);
 		}
 
 		public CustomFontTextView (Context context, IAttributeSet attrs): base(context, attrs) {
-			ApplyCustomFont (context, attrs);
+			CustomFontHelper.ApplyCustomFont (this, context, attrs);
 		}
 
 		public CustomFontTextView (Context context, IAttributeSet attrs, int defStyle): base(context, attrs, defStyle) {
-			ApplyCustomFont (context, attrs);
-		}
-
-		//applies font from XML
-		private void ApplyCustomFont(Context context, IAttributeSet attrs) {
-			TypedArray attributeArray = context.ObtainStyledAttributes (attrs, Resource.Styleable.CustomFont);
-
-			string fontName = attributeArray.GetString (Resource.Styleable.CustomFont_font);
-			int textStyle = attrs.GetAttributeIntValue (ANDROID_SCHEMA, "textStyle", 0);
-
-			Typeface customFont = SelectTypeface (context, fontName);
-
-			switch (textStyle) {
-			case 1:
-				SetTypeface (customFont, TypefaceStyle.Bold);
-				break;
-			case 2:
-				SetTypeface (customFont, TypefaceStyle.Italic);
-				break;
-			case 3:
-				SetTypeface (customFont, TypefaceStyle.BoldItalic);
-				break;
-			default:
-				SetTypeface (customFont, TypefaceStyle.Normal);
-				break;
-			}
-
-			attributeArray.Recycle ();
-		}
-
-		//returns correct Typeface from fontName
-		private Typeface SelectTypeface(Context context, String fontName) {
-			if (fontName.Equals (context.GetString (Resource.String.Verveine))) {
-				return Typeface.CreateFromAsset (context.Assets, "fonts/Verveine W01 Regular.ttf");
-			} else if (fontName.Equals (context.GetString (Resource.String.DIN_light))) {
-				return Typeface.CreateFromAsset (context.Assets, "fonts/DINPro-Light.ttf");
-			} else if (fontName.Equals (context.GetString (Resource.String.DIN_regular))) {
-				return Typeface.CreateFromAsset (context.Assets, "fonts/DINPro-Regular.ttf");
-			} else {
-				return null;
-			}
+			CustomFontHelper.ApplyCustomFont (this, context, attrs);
 		}
 	}
 }
