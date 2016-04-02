7aab7719ef7a3bddd458c047dafa2f2f9e1ec3d4
Crejer <crevits.j@gmail.com>
2016-04-02 19:01:38 +0200

    main screen

--- CrossPlatform/TotemApp/TotemAppIos/SGV/TotemApp/Controllers/MainViewController.cs
+++ CrossPlatform/TotemApp/TotemAppIos/SGV/TotemApp/Controllers/MainViewController.cs
@@ -7,34 +7,84 @@ namespace TotemAppIos
 {
 	public partial class MainViewController : UIViewController
 	{
+
+		#region delegates
+
+		#endregion
+
+		#region variables
 		AppController _appController = AppController.Instance;
+		#endregion
+
+		#region constructor
 		public MainViewController () : base ("MainViewController", null)
 		{
 		}
-		public override void ViewDidLoad ()
-		{
-			base.ViewDidLoad ();
-			btnClick.TouchUpInside+= BtnClickTouchUpInside;
-			_appController.updateLabelEvent+= _appController_updateLabelEvent;
-			// Perform any additional setup after loading the view, typically from a nib.
-		}
+		#endregion
 
-		void _appController_updateLabelEvent (int timesClicked)
-		{
-			lblClicked.Text = string.Format ("clicked {0} times",timesClicked);
-		}
+		#region properties
 
-		void BtnClickTouchUpInside (object sender, EventArgs e)
-		{
-			Console.WriteLine (_appController.Totems[0].title);
-			lblClicked.Text = _appController.Eigenschappen [0].name;
-		}
+		#endregion
+
+		#region public methods
+
+		#region overrided methods
 
 		public override void DidReceiveMemoryWarning ()
 		{
 			base.DidReceiveMemoryWarning ();
 			// Release any cached data, images, etc that aren't in use.
 		}
+		public override UIStatusBarStyle PreferredStatusBarStyle ()
+		{
+			return UIStatusBarStyle.LightContent;
+		}
+		#region viewlifecycle
+
+		public override void ViewDidLoad ()
+		{
+
+			base.ViewDidLoad ();
+			foreach (var fontFamily in UIFont.FamilyNames) {
+				Console.WriteLine (fontFamily+":");
+				foreach (var fontName in UIFont.FontNamesForFamilyName (fontFamily)) {
+					Console.WriteLine ("     "+fontName);
+				}
+			}
+			lblTitle.Text = "TOTEMAPP";
+			imgMountain.Image = UIImage.FromBundle ("SharedAssets/Berg");
+			imgTotem.Image = UIImage.FromBundle ("SharedAssets/Totem");
+
+			lblTotemsButton.Text = "TOTEMS";
+			btnTotems.RippleColor = UIColor.FromRGB (0, 68, 116);
+			lblEigenschappenButton.Text = "EIGENSCHAPPEN";
+			btnEigenschappen.RippleColor = UIColor.FromRGB (0, 68, 116);
+			lblProfielenButton.Text = "PROFIELEN";
+			btnProfielen.RippleColor = UIColor.FromRGB (0, 68, 116);
+			lblChecklistButton.Text = "TOTEMISATIE CHECKLIST";
+			btnChecklist.RippleColor = UIColor.FromRGB (0, 68, 116);
+			//imgReturn.Image = UIImage.FromBundle ("SharedAssets/arrow_back_white");
+
+
+
+
+
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+			
+		#endregion
+
+		#endregion
+
+		#endregion
+
+		#region private methods
+
+		#endregion
+
+
+
+
 	}
 }
 
---
+++ CrossPlatform/TotemApp/TotemAppIos/SGV/TotemApp/IosUtils.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TotemAppIos
+{
+	public class IosUtils
+	{
+		public const string DINPRO_Reg = "DINPro-Regular";
+		public const string DINPRO_Light = "DINPro-Light";
+		public const string SKETCH = "SketchBlock-Light";
+		public const string VERVEINE = "VerveineW01-Regular";
+	}
+}
+
