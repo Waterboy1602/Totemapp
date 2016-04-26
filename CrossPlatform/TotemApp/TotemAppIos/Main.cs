258c54ae7d74b46bfb6d2182778608cb0305788c
Frederick Eskens <frederickeskens@gmail.com>
2016-04-26 16:40:29 +0200

    Improved loading dialog (iOS)

--- CrossPlatform/TotemApp/TotemAppIos/Main.cs
+++ CrossPlatform/TotemApp/TotemAppIos/Main.cs
@@ -1,20 +1,9 @@
-﻿//
-// Main.cs
-//
-// Author:
-//       Scouts en Gidsen Vlaanderen <informatica@scoutsengidsenvlaanderen.be>
-//
-// Copyright (c) 2016 SGV
-//
-using UIKit;
+﻿using UIKit;
 
-namespace TotemAppIos
-{
-	public class Application
-	{
+namespace TotemAppIos {
+	public class Application {
 		// This is the main entry point of the application.
-		static void Main (string[] args)
-		{
+		static void Main (string[] args) {
 			// if you want to use a different Application Delegate class from "AppDelegate"
 			// you can specify it here.
 			UIApplication.Main (args, null, "AppDelegate");
