1dd579d0acd5940aa81917252e6e23dfce894e1e
fe <fe@PC2012-194>
2016-04-28 16:40:12 +0200

    Incremented database version (Core)

--- CrossPlatform/TotemApp/TotemAppCore/SGV/DL/Database.cs
+++ CrossPlatform/TotemApp/TotemAppCore/SGV/DL/Database.cs
@@ -22,7 +22,7 @@ namespace TotemAppCore {
 		string originalDBLocation = "SharedAssets/totems.sqlite";
         #endif
 
-        const int DATABASE_VERSION = 1;
+        const int DATABASE_VERSION = 2;
 		string currentDBName = "totems" + DATABASE_VERSION + ".sqlite";
         string oldDBName = "totems" + (DATABASE_VERSION - 1) + ".sqlite";
 
